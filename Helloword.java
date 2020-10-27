cde4dc1c998f324e037b0493c5a7e5f0e0d33b52
JGlass25 <73563194+JGlass25@users.noreply.github.com>
2020-10-27 19:42:12 -0400

    Add files via upload

---
+++ Helloword.java
@@ -0,0 +1,13 @@
+
+public class Helloword {
+
+	public static void main(String[] args) {
+		
+		byte a = 13;
+		byte b = 6;
+		byte r = (byte)(a*b);
+		System.out.println(Integer.toBinaryString(r));
+		
+	}
+
+}
